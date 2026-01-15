e9585a7cf35fb40209695ee4828ba5ab802b1506
Willian <willianc@pm.me>
2026-01-14 22:48:37 -0300

    another small cleanup

--- exporter/duckdbexporter/config.go
+++ exporter/duckdbexporter/config.go
@@ -10,59 +10,10 @@ import (
 	"go.opentelemetry.io/collector/confmap"
 )
 
-const (
-	rotationFieldName = "rotation"
-	// backupsFieldName  = "max_backups"
-)
-
-var (
-	errInvalidOctal          = errors.New("directory_permissions value must be a valid octal representation")
-	errInvalidPermissionBits = errors.New("directory_permissions contain invalid bits for file access")
-	errDirPermsRequireCreate = errors.New("directory_permissions requires create_directory to be true")
-)
-
 // Config defines configuration for file exporter.
 type Config struct {
 	Enabled                    bool  `mapstructure:"enabled"`
 	directoryPermissionsParsed int64 `mapstructure:"-"`
-}
-
-// Rotation an option to rolling log files
-type Rotation struct {
-	// MaxMegabytes is the maximum size in megabytes of the file before it gets
-	// rotated. It defaults to 100 megabytes.
-	MaxMegabytes int `mapstructure:"max_megabytes"`
-
-	// MaxDays is the maximum number of days to retain old log files based on the
-	// timestamp encoded in their filename.  Note that a day is defined as 24
-	// hours and may not exactly correspond to calendar days due to daylight
-	// savings, leap seconds, etc. The default is not to remove old log files
-	// based on age.
-	MaxDays int `mapstructure:"max_days" `
-
-	// MaxBackups is the maximum number of old log files to retain. The default
-	// is to 100 files.
-	MaxBackups int `mapstructure:"max_backups" `
-
-	// LocalTime determines if the time used for formatting the timestamps in
-	// backup files is the computer's local time.  The default is to use UTC
-	// time.
-	LocalTime bool `mapstructure:"localtime"`
-}
-
-type GroupBy struct {
-	// Enables group_by. When group_by is enabled, rotation setting is ignored.  Default is false.
-	Enabled bool `mapstructure:"enabled"`
-
-	// ResourceAttribute specifies the name of the resource attribute that
-	// contains the path segment of the file to write to. The final path will be
-	// the Path config value, with the * replaced with the value of this resource
-	// attribute. Default is "fileexporter.path_segment".
-	ResourceAttribute string `mapstructure:"resource_attribute"`
-
-	// MaxOpenFiles specifies the maximum number of open file descriptors for the output files.
-	// The default is 100.
-	MaxOpenFiles int `mapstructure:"max_open_files"`
 }
 
 var _ component.Config = (*Config)(nil)
@@ -75,7 +26,7 @@ func (cfg *Config) Validate() error {
 // Unmarshal a confmap.Conf into the config struct.
 func (cfg *Config) Unmarshal(componentParser *confmap.Conf) error {
 	if componentParser == nil {
-		return errors.New("empty config for file exporter")
+		return errors.New("empty config for duckdb exporter")
 	}
 	// first load the config normally
 	err := componentParser.Unmarshal(cfg)
