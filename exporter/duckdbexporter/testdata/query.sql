16c16e72fe462e7e7ab2e9201c0800b400daa910
Willian Carvalho <willianc@protonmail.com>
2026-01-15 23:39:13 -0300

    added events table

--- exporter/duckdbexporter/testdata/query.sql
+++ exporter/duckdbexporter/testdata/query.sql
@@ -1,20 +1,28 @@
 -- TODO: events table with event_name, event_timestamp, event_attributes(map varchar, varchar)
 
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS spans;
 
-CREATE TABLE spans (
-    span_name varchar,
-    span_id varchar,
+-- DROP TABLE IF EXISTS spans;
+
+CREATE OR REPLACE TABLE spans (
+    name varchar,
+    id varchar primary key,
     parent_id varchar,
     trace_id varchar,
-    span_kind uinteger,
+    kind uinteger,
     schema_url varchar,
     resources map(varchar, varchar),
     resource_scope varchar,
     start_timestamp timestamp,
     end_timestamp timestamp,
     flags uinteger,
+);
+
+CREATE OR REPLACE TABLE events (
+    span_id varchar,
+    foreign key(span_id) references spans(id),
+    name varchar,
+    attributes map(varchar, varchar)
 );
 
 COMMIT;
